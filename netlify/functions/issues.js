3036de80fcf9749cfadc8d05f4b9b37e1f1d0f5a
horeaporutiu <horeaporutiu@gmail.com>
2023-10-02 16:20:15 +0200

    chnage miroAppCardId:: to numeric

--- netlify/functions/issues.js
+++ netlify/functions/issues.js
@@ -33,7 +33,7 @@ exports.handler = async function (event) {
   const { data, error } = await supabase
     .from("card-mapping")
     .select(
-      "id, miroAppCardId::numeric, gitHubIssueId, miroUserId::text, gitHubUsername, created_at, miroBoardId, gitHubIssueNumber, auth ( access_token )",
+      "id, miroAppCardId, gitHubIssueId, miroUserId, gitHubUsername, created_at, miroBoardId, gitHubIssueNumber, auth ( access_token )",
     )
     .eq("gitHubIssueId", gitHubIssueId);
 
